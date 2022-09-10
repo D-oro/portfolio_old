6a3b89586badca9473b3da274262f84eb1177b9e
D-oro <107217252+D-oro@users.noreply.github.com>
2022-09-10 10:39:42 +0100

    added main sections

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import apple from './images/apple.png'
+import portrait from './images/portrait.png'
+import berries from './images/berries.png'
+import sky from './images/sky.png'
+import flower from './images/flower.png'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+      <header>
+        <div id="square"><img src={portrait} id="portrait" alt="portrait of author"/></div>
+        <text id="headertext">Portfolio Doro Hinrichs</text>
       </header>
+
+      <main>
+        <article id="maintext">Psychologist graduate turned Junior Software Developer through 16 week intensive coding bootcamp with CodeClan from July-October 2022.</article>
+        <img src={flower} id="picture" alt="an apple on a tree"/>
+        <aside id="aside">
+          Proficient in:
+          <ul>
+            <li>Python</li>
+            <li>Javascript</li>
+            <li>Java</li>
+            <li>CSS</li>
+            <li>HTML</li>
+            <li>React</li>
+          </ul>
+          </aside>
+      </main>
+
+      <footer>
+        Github
+        LinkedIn
+      </footer>
+    
     </div>
   );
 }
